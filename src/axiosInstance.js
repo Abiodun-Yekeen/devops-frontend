47aff43fce1b0dc9564ff8eca3a09a6dc9fbc3ec
Yekeen Abiodun <abiodunyekeen76@gmail.com>
2024-01-01 18:14:00 +0000

    update

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.88.210.223:8085/', // Set your backend server URL here
+  baseURL: '/api/', // Set your backend server URL here
 });
 
 export default axiosInstance;
